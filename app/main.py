e7f744e4208329682220c84024ea99b4d0d445ef
shroudhek <hemalkavaiya@gmail.com>
2021-05-27 13:16:04 +0530

    added runtime.txt

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def bot():
     responded = False
 
     print(incoming_msg)
-    if 'Hi' in incoming_msg or 'Hey' in incoming_msg or 'Heya' in incoming_msg or 'Menu' in incoming_msg:
+    if 'hi' in incoming_msg or 'hey' in incoming_msg or 'heya' in incoming_msg or 'menu' in incoming_msg:
         text = f'Hello 🙋🏽‍♂, \nThis is a Cowin-Bot developed by https://github.com/HemalKavaiya to provide latest information about vaccination.\n\n Please enter one of the following option 👇 \n *slots*. It will check for available slots*.'
         msg.body(text)
         responded = True
